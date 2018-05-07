13522ee39c0b8feee2505267852442b5cc9e694e
Br1 <brunogiovagnoli@gmail.com>
2018-05-07 16:29:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const EventEmitter = require("events");
+const util = require("util");
+
+// Traditional example --------------------------
+
+function Greetr() {
+    this.greeting = "Hello friend!";
+}
+
+util.inherits(Greetr, EventEmitter);
+
+Greetr.prototype.greet = function(data) {
+    console.log(this.greeting + ": " + data);
+    this.emit("greet", data);
+}
+
+var greeter1 = new Greetr();
+
+greeter1.on("greet", function(data) {
+    console.log("Someone greeted!: " + data);
+});
+
+greeter1.greet("Bruno");
+
+// ES6 example ----------------------------------
+
+class Greetr2 extends EventEmitter {
+
+    constructor() {
+        super();
+        this.greeting = "Hello friend!";
+    }
+
+    greet(data) {
+        console.log(this.greeting + ": " + data);
+        this.emit("greet", data);
+    }
+}
+
+const greeter2 = new Greetr2();
+
+greeter2.on("greet", (data) => {
+    console.log(`Someone greeted!: ${data}`);
+});
+
+greeter2.greet("Bruno");
